3e9f83ce3a2c10cf81056f45258be32c37795676
panteliselef <panteliselef@outlook.com>
2023-03-07 16:33:50 +0200

    chore(clerk-react): Additional tests for `IsomorphicClerkOptions`

--- packages/react/src/types._test.ts
+++ packages/react/src/types._test.ts
@@ -6,26 +6,84 @@ import type { IsomorphicClerkOptions } from './types';
 
 // @ts-ignore
 let props: IsomorphicClerkOptions;
+
+// @ts-expect-error
+props = {};
+
 // @ts-expect-no-error
 props = {
   frontendApi: '',
 };
+
+// @ts-expect-no-error
+props = {
+  publishableKey: '',
+};
+
 // @ts-expect-error
 props = {
   frontendApi: '',
   publishableKey: '',
 };
+
 // @ts-expect-error
-props = {};
-// @ts-expect-no-error
 props = {
   domain: '',
   publishableKey: '',
+};
+
+// @ts-expect-no-error
+props = {
+  publishableKey: '',
+  isSatellite: true,
+  domain: '',
 };
 
 // @ts-expect-error
 props = {
   publishableKey: '',
   domain: '',
+  proxyUrl: '',
+};
+
+// @ts-expect-error
+props = {
+  isSatellite: true,
+  publishableKey: '',
+  domain: '',
+  proxyUrl: '',
+};
+
+// @ts-expect-error
+props = {
+  isSatellite: true,
+  publishableKey: '',
+};
+
+// @ts-expect-error
+props = {
+  isSatellite: _ => true,
+  publishableKey: '',
+  domain: '',
+  proxyUrl: '',
+};
+
+// @ts-expect-no-error
+props = {
+  isSatellite: _ => true,
+  publishableKey: '',
+  domain: '',
+};
+
+// @ts-expect-no-error
+props = {
+  publishableKey: '',
+  proxyUrl: '',
+};
+
+// @ts-expect-no-error
+props = {
+  isSatellite: _ => true,
+  publishableKey: '',
   proxyUrl: '',
 };
